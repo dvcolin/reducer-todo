a6b210e7fea8444072ff364dc1e280250172592e
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-08-13 13:46:12 -0700

    added onChange handler in TodoForm

--- project/src/App.js
+++ project/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
 
 function App() {
   return (
     <div className="App">
+      <TodoForm />
       <TodoList />
     </div>
   );
